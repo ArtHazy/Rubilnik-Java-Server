edfe223e2ab6266a85ce9f8d76f5068af9f8d53b
Art Monakhov <artemius135@gmail.com>
2024-10-19 17:08:09 +0300

    Java only. Goodbye, React

--- UI-server/src/_codux/boards/view-login/view-login.board.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { createBoard } from "@wixc3/react-board";
-import { ViewLogin } from "../../../1 Authentication/ViewLogin.jsx";
-
-export default createBoard({
-  name: "ViewLogin",
-  Board: () => <ViewLogin />,
-  isSnippet: true,
-});
